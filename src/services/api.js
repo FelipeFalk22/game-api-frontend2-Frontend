a9a8495ea5ab5e0d385dfb8c0c6d56c372de84f1
Felipe Falk <felipe.brf@aluno.ifsc.edu.br>
2025-12-02 21:24:46 -0300

    Add files via upload

---
+++ src/services/api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'https://backend-api-players.onrender.com', // Certifique-se que a porta é a mesma do seu back-end
+});
+
+// Adiciona um interceptor para incluir o token de autenticação em todas as requisições
+api.interceptors.request.use(async (config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+
+export default api;
